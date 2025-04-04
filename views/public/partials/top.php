593a4edd954e7c43c7ac3acd4b6b77456b448e36
Dejan Zivkovic <zile028@gmail.com>
2025-04-04 15:24:20 +0200

    Update top.php

--- views/public/partials/top.php
+++ views/public/partials/top.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="/upload/favicon.png">
+    <link rel="icon" href="/uploads/favicon.png">
     <title>ВПШ - Земун</title>
     <!-- Font Awesome 5 | visit: https://fontawesome.com/v5.15/icons?d=gallery&p=2 -->
 
